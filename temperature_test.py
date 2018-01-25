d5560162152634ea271ba5fe6e96e61d9712b1a5
linuxvfast <2559811312@qq.com>
2018-01-25 11:49:05 +0800

    显示高低温度的相差值

--- temperature_test.py
+++ temperature_test.py
@@ -5,12 +5,13 @@ from datetime import datetime
 
 filename = 'temperatures.csv'
 with open(filename) as file:
-    reads = csv.reader(file)
-    header_row = next(reads)
+    reads = csv.reader(file)  #全部读取
+    header_row = next(reads)  #获取下一行的内容
 
+    #最高温度，日期，最低温度
     tem,date,lows= [],[],[]
     for row_num in reads:
-        current_date = datetime.strptime(row_num[0],'%Y-%m-%d')
+        current_date = datetime.strptime(row_num[0],'%Y-%m-%d')  #转换日期
         date.append(current_date)
 
         row = int(row_num[2])
@@ -21,8 +22,11 @@ with open(filename) as file:
     # print(tem)
     # print(date)
 # fig = plt.figure(dpi=128,figsize=(10,6))
-plt.plot(date,tem,c='blue')
-plt.plot(date,lows,c='red')
+#alpha 表示颜色的透明度
+plt.plot(date,tem,c='blue',alpha=0.5)
+plt.plot(date,lows,c='red',alpha=0.5)
+plt.fill_between(date,tem,lows,facecolor='blue',alpha=0.1)  #facecolor 表示填充的颜色
+
 #装饰
 plt.title('temperatures hight',fontsize=24)
 plt.xlabel('',fontsize=16)
